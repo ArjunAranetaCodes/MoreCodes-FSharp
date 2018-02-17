84933857ad2f13d330c17fde3751f22b785f5921
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-17 08:26:45 +0800

    2018 Updates

---
+++ Conversions/problem12.fs
@@ -0,0 +1,7 @@
+//Problem 12: Write a program that converts numbers of day to seconds.
+let mutable day = 1
+let seconds = day * 24 * 60 * 60
+
+printf "%d" seconds
+
+System.Console.ReadKey() |> ignore

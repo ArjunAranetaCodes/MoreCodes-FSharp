a76256a1683c4c66fae083374f6adc6031b22440
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-20 07:18:50 +0800

    2018 Updates

---
+++ Conversions/problem14.fs
@@ -0,0 +1,7 @@
+//Problem 14: Write a program that converts lower case to upper case characters.
+let mutable str = "abc"
+str <- str.ToUpper()
+
+printf "%s" str
+
+System.Console.ReadKey() |> ignore

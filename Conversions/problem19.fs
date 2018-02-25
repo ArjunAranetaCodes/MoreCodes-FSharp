f48b014a226bfca4b94b220815d71b34263e9b86
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-02-25 08:08:49 +0800

    2018 Updates

---
+++ Conversions/problem19.fs
@@ -0,0 +1,12 @@
+//Problem 19: Write a program that converts numbers to words.
+let mutable word = "program"
+let vowels = "aeiou"
+let mutable vowelCount = 0
+
+for x = 0 to word.Length - 1 do
+ if(vowels.Contains(word.[x..x])) then
+  vowelCount <- vowelCount + 1
+
+printfn "Total: %i" vowelCount
+System.Console.ReadKey() |> ignore
+

615d127522838c5e8a34cb728fa1c02f37782407
ArjunAranetaCodes <arjunaraneta@gmail.com>
2018-06-11 08:19:48 +0800

    2018 Updates

--- Regular Expressions/problem12.fs
+++
@@ -1,9 +0,0 @@
-//Problem 12: Write a program that counts the occurrence of digits in a string using Regular Expression.
-open System.Text.RegularExpressions
-
-let regex = @"\D"
-let result1 = Regex.Replace("Hello12 World12", regex, "").Length
-printfn "%s" (result1.ToString())
-
-System.Console.ReadKey() |> ignore
-

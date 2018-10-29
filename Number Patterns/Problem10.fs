a34e8aaca1a4f827513200c91a2467debffc00fb
Arjun Araneta <arjunaraneta@gmail.com>
2018-10-29 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem10.fs
@@ -0,0 +1,20 @@
+﻿(*
+Problem 10: Write a program to print the number pattern using loop.
+12345
+23456
+34567
+45678
+56789
+*)
+let mutable row = 5
+let mutable col = 5
+let mutable min = 1
+
+for y = 0 to 4 do
+ let mutable num = min + y
+ for x = 0 to 4 do
+  printf "%i" num
+  num <- num + 1
+ printfn ""
+
+System.Console.ReadKey() |> ignore

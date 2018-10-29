e3a746dda5a95302109f7bde671a9657919e02c5
Arjun Araneta <arjunaraneta@gmail.com>
2018-10-29 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem8.fs
@@ -0,0 +1,20 @@
+﻿(*
+Problem 8: Write a program to print the number pattern of ones and zeros using loop.
+11111
+11111
+11011
+11111
+11111
+*)
+
+let mutable row = 5
+let mutable col = 5
+for y = 0 to 4 do
+ for x = 0 to 4 do
+  if(x = (row / 2) && y = (col / 2)) then
+   printf "0"
+  else
+   printf "1"
+ printfn ""
+
+System.Console.ReadKey() |> ignore

ad174e9cc0ac5b399232ec193ae30d36155b6fce
Arjun Araneta <arjunaraneta@gmail.com>
2018-10-29 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem6.fs
@@ -0,0 +1,18 @@
+﻿(*
+Problem 6: Write a program to print the number pattern of ones and zeros using loop.
+11111
+10001
+10001
+10001
+11111
+*)
+
+for y = 0 to 4 do
+ for x = 0 to 4 do
+  if((y = 0) || (y = 4) || (x = 0) || (x = 4)) then
+   printf "1"
+  else
+   printf "0"
+ printfn ""
+
+System.Console.ReadKey() |> ignore

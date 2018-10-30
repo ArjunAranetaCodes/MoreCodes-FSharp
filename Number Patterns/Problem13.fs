f1770d1092e9ece3e2b7d112702a7150ba71d188
Arjun Araneta <arjunaraneta@gmail.com>
2018-10-30 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem13.fs
@@ -0,0 +1,25 @@
+﻿(*
+Problem 13: Write a program to print the pattern of asterisks using loop.
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+*)
+
+let row = 4
+for y = 0 to row do
+ for x = 0 to y do
+  printf "*"
+ printfn ""
+
+for y = row - 1 downto 0 do
+ for x = 0 to y do
+  printf "*"
+ printfn ""
+
+System.Console.ReadKey() |> ignore

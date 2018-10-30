6e7fb702c7967668d56cd42ccf50198127743e2f
Arjun Araneta <arjunaraneta@gmail.com>
2018-10-30 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem15.fs
@@ -0,0 +1,17 @@
+﻿(*
+Problem 15: Write a program to print the number pattern using loop.
+11111
+2222
+333
+44
+5
+*)
+let mutable row = 4
+let mutable num = 1
+for y = row downto 0 do
+ for x = 0 to y do
+  printf "%i" num
+ num <- num + 1
+ printfn ""
+
+System.Console.ReadKey() |> ignore

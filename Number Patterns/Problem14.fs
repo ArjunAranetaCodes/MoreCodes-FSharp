5d91649c3762f72e45aa868fe5bed1505442519b
Arjun Araneta <arjunaraneta@gmail.com>
2018-10-30 05:00:00 +0100

    2018 Updates

---
+++ Number Patterns/Problem14.fs
@@ -0,0 +1,15 @@
+﻿(*
+Problem 14: Write a program to print the number pattern using loop.
+1
+22
+333
+4444
+55555
+*)
+
+for y = 1 to 5 do
+ for x = 1 to y do
+  printf "%i" y
+ printfn ""
+
+System.Console.ReadKey() |> ignore
